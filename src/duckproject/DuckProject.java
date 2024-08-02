bdb583ab3ddc442da55e5cf102143f6253661a22
Anomali <anomaliscript@gmail.com>
2024-08-02 09:22:14 -0700

    making keybinds and gravity

--- src/duckproject/DuckProject.java
+++ src/duckproject/DuckProject.java
@@ -1,5 +1,6 @@
 package duckproject;
 import processing.core.*;
+import java.awt.event.*;
 
 public class DuckProject extends PApplet {
     PImage duck;
@@ -16,6 +17,7 @@ public class DuckProject extends PApplet {
     int obstacleY = 330;
 
     float speed = 8;
+    int jumpHeight;
 
     public static void main(String[] args) {
         PApplet.main("duckproject.DuckProject");
@@ -27,6 +29,9 @@ public class DuckProject extends PApplet {
 
     public void setup() {
         duck = loadImage("Images/Duck.png");
+        // Translucent
+        image(duck, 0, 0);
+        tint(255, 126);
         background = loadImage("Images/Sky.png");
         obstacle = loadImage("Images/Hydrant.png");
     }
@@ -34,11 +39,18 @@ public class DuckProject extends PApplet {
     public void draw() {
         drawBackground();
         drawDuck();
+        createObstacles();
+        obstacle.resize(50, 0);
     }
 
     public void drawDuck() {
         imageMode(CENTER);
         image(duck, duckX, duckY);
+        if (duckY <= 300) {
+            // Higher the number, higher the gravity
+            jumpHeight += 1;
+            duckY += jumpHeight;
+        }
     }
 
     public void drawBackground() {
@@ -49,5 +61,26 @@ public class DuckProject extends PApplet {
         if (bgX <= (background.width*-1)) {
             bgX = 0;
         }
+    }
+
+    public void keyPressed() {
+        if (key == ' ' || keyCode == UP) {
+            if (duckY >= 300) {
+                jumpHeight = -15;
+                duckY += jumpHeight;
+            }
+        }
+    }
+
+    public void createObstacles() {
+        imageMode(CENTER);
+        image(obstacle, obstacleX, obstacleY);
+        // Parallax
+        obstacleX -= (speed * 1.5);
+        if (obstacleX < 0) {
+            // Spawning
+            obstacleX = width;
+        }
+
     }
 }
