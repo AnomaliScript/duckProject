29ab5004ba243b4e2708e7cd8919ca8968bc1ae4
Anomali <anomaliscript@gmail.com>
2024-08-02 11:25:15 -0700

    worked though a movement bug

--- src/duckproject/DuckProject.java
+++ src/duckproject/DuckProject.java
@@ -1,23 +1,36 @@
 package duckproject;
 import processing.core.*;
-import java.awt.event.*;
+import java.util.*;
 
 public class DuckProject extends PApplet {
+    Random rand = new Random();
+
     PImage duck;
     PImage background;
-    PImage obstacle;
+    PImage obstacleImg;
 
     int duckX = 70;
     int duckY = 300;
 
+    int bgSpeed = 8;
     int bgX = 0;
     int bgY = 0;
 
-    int obstacleX;
-    int obstacleY = 330;
+    int numObstacles = 5;
+    // Note that the starting spawn point is at the right edge of the screen
+    int closest = 0;
+    int farthest = 200;
 
-    float speed = 8;
+    ArrayList<Double> speeds = new ArrayList<Double>();
     int jumpHeight;
+
+    int startTime;
+    int timer;
+
+    ArrayList<obstacle> obstacles = new ArrayList<obstacle>();
+
+    int mercyHorizontal = 40;
+    int mercyVertical = 40;
 
     public static void main(String[] args) {
         PApplet.main("duckproject.DuckProject");
@@ -29,18 +42,25 @@ public class DuckProject extends PApplet {
 
     public void setup() {
         duck = loadImage("Images/Duck.png");
-        // Translucent
         image(duck, 0, 0);
-        tint(255, 126);
         background = loadImage("Images/Sky.png");
-        obstacle = loadImage("Images/Hydrant.png");
+        obstacleImg = loadImage("Images/Hydrant.png");
+        // Filling in the ArrayList of obstacle objects and their speeds
+        for (int i = 0; i < numObstacles; i++) {
+            obstacle obstacleInstance = new obstacle(
+                    width + rand.nextInt((farthest - closest) + 1) + closest,
+                    6 + (10 - 6) * rand.nextDouble()
+            );
+            obstacles.add(obstacleInstance);
+            System.out.println(i + "x: " + obstacles.get(i).X);
+        }
     }
 
     public void draw() {
         drawBackground();
         drawDuck();
+        obstacleImg.resize(50, 0);
         createObstacles();
-        obstacle.resize(50, 0);
     }
 
     public void drawDuck() {
@@ -57,7 +77,7 @@ public class DuckProject extends PApplet {
         imageMode(CORNER);
         image(background, bgX, bgY);
         image(background, bgX + background.width, 0);
-        bgX -= speed;
+        bgX -= bgSpeed;
         if (bgX <= (background.width*-1)) {
             bgX = 0;
         }
@@ -74,13 +94,18 @@ public class DuckProject extends PApplet {
 
     public void createObstacles() {
         imageMode(CENTER);
-        image(obstacle, obstacleX, obstacleY);
-        // Parallax
-        obstacleX -= (speed * 1.5);
-        if (obstacleX < 0) {
-            // Spawning
-            obstacleX = width;
+        for (int i = 0; i < obstacles.size(); i++) {
+            // Parallax
+            obstacles.get(i).X -= obstacles.get(i).speed;
+            if (obstacles.get(i).X < 0) {
+                // Spawning (technically moving it instantaneously)
+                obstacles.get(i).X = width; // + rand.nextInt((farthest - closest) + 1) + closest;
+            }
+            // AABB
+            if ((abs(duckX - obstacles.get(i).X) < mercyHorizontal) && (abs(duckY - obstacles.get(i).Y)) < mercyVertical) {
+                System.out.println("Game Over!");
+            }
+            image(obstacleImg, obstacles.get(i).X, obstacles.get(i).Y);
         }
-
     }
 }
---
+++ src/duckproject/obstacle.java
@@ -0,0 +1,14 @@
+package duckproject;
+
+
+public class obstacle {
+    int X;
+    int Y;
+    double speed;
+
+    public obstacle(int X, double speed) {
+        this.X = X;
+        this.Y = 330;
+        this.speed = speed;
+    }
+}
